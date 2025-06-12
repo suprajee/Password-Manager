f2343a02f07645c23dafac92322793522b3c6102
Suprajeet Suman <suprajeet30032006@gmail.com>
2025-06-12 17:53:40 +0530

    changes

--- crypto.js
+++
@@ -1,9 +0,0 @@
-function encrypt(data, key) {
-    // Simple encryption (use a library like CryptoJS for real apps)
-    return btoa(data);
-  }
-  
-  function decrypt(data, key) {
-    return atob(data);
-  }
-  
